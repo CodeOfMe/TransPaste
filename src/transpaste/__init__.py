54da9f86adf3ddfb7d1582da69576f3054919be5
akino <6130092+cycleuser@users.noreply.github.com>
2026-03-19 13:56:51 +0800

    Update __init__.py

--- src/transpaste/__init__.py
+++ src/transpaste/__init__.py
@@ -1,5 +1,5 @@
 from .main import main, LANGUAGE_MAP, TRANSLATION_STYLES, LENGTH_OPTIONS
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = ["main", "LANGUAGE_MAP", "TRANSLATION_STYLES", "LENGTH_OPTIONS"]
